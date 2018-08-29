672ac683945675ef45949a7dbd79b460469540eb
李佳航 <1191039404@qq.com>
2018-08-29 14:41:45 +0800

    Update users.js

--- src/pages/users/services/users.js
+++ src/pages/users/services/users.js
@@ -1,5 +1,5 @@
 import { PAGE_SIZE } from '../constants';
-import request from '../../../utils/user';
+import request from '../../../utils/request';
 
 export function fetch({ page = 1 }) {
   return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
